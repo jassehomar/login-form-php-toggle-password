051a148b5cb8376e8402096ce4427bfc5f09720f
Omar Jasseh <jassehomar99@gmail.com>
2019-05-28 23:36:53 +0000

    add login processor

---
+++ process-login.php
@@ -0,0 +1,20 @@
+<?php
+
+session_start();
+
+if(isset($_POST['toggler'])){
+
+    if($_POST['toggler'] == 'show'){
+        $_SESSION['password_field_type'] = 'text';
+        $_SESSION['toggle'] = 'hide';
+    }else{
+        $_SESSION['password_field_type'] = 'password';
+        $_SESSION['toggle'] = 'show';
+    }
+   
+    $_SESSION['email'] = $_POST['email'];
+    $_SESSION['password'] = $_POST['password'];
+    
+}
+header('Location: responsive-login-with-show-and-hide-password.php');
+exit;
